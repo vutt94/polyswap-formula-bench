6cf7117275c0c558dde095cfb2f55dca229a1484
Tran Vu <vutt94@gmail.com>
2022-04-05 01:24:38 +0700

    Done Formula Bench

--- contracts/PolySwapFormula.sol
+++ contracts/PolySwapFormula.sol
@@ -8,17 +8,23 @@ contract PolySwapFormula {
     uint256[2][] calldata parameters,
     uint256 k,
     uint256 initialX
-  ) external pure returns (uint256[] memory results) {
+  ) external view returns (uint256[] memory results) {
     results = new uint256[](100);
     uint256 nextX = FullMath.toPrecision(initialX, 18);
     uint256 curX;
     uint256 count = 0;
     do {
       curX = nextX;
-      (uint256 fx, ) = f(parameters, k, curX, 36, 18);
-      uint256 derivativeFx = derivativeF(parameters, curX, 54, 18);
+      (uint256 fx, bool isFxNegative) = f(parameters, k, curX, 36, 18);
 
-      nextX = curX + FullMath.toPrecision(fx, 18) / derivativeFx;
+      if (isFxNegative) {
+        break;
+      }
+
+      uint256 dFx = derivativeF(parameters, curX, 54, 18);
+
+      nextX = curX + FullMath.toPrecision(fx, 18) / dFx;
+
       results[count] = nextX;
       count++;
     } while (curX != nextX);
@@ -35,17 +41,41 @@ contract PolySwapFormula {
     uint256 count = 0;
     do {
       curX = nextX;
-      (uint256 fx, bool isNegative) = f(parameters, k, curX, 36, 18);
+      (uint256 fx, bool isFxNegative) = f(parameters, k, curX, 36, 18);
       uint256 derivativeFx = derivativeF(parameters, curX, 55, 18);
       uint256 doubleDerivativeFx = doubleDerivativeF(parameters, curX, 74, 18);
+
       uint256 upperPart = 2 * fx * derivativeFx;
       uint256 lowerPart;
+      bool lowerPartNegative;
 
-      if (isNegative) {
-        lowerPart = 2 * FullMath.pow2(derivativeFx) + fx * doubleDerivativeFx;
+      if (isFxNegative) {
+        if (fx * doubleDerivativeFx >= 2 * FullMath.pow2(derivativeFx)) {
+          lowerPartNegative = false;
+        } else {
+          lowerPartNegative = true;
+        }
+
+        lowerPart = FullMath.subAbs(
+          fx * doubleDerivativeFx,
+          2 * FullMath.pow2(derivativeFx)
+        );
+      } else {
+        if (2 * FullMath.pow2(derivativeFx) >= fx * doubleDerivativeFx) {
+          lowerPartNegative = false;
+        } else {
+          lowerPartNegative = true;
+        }
+
+        lowerPart = FullMath.subAbs(
+          2 * FullMath.pow2(derivativeFx),
+          fx * doubleDerivativeFx
+        );
+      }
+
+      if (lowerPartNegative) {
         nextX = curX - FullMath.toPrecision(upperPart, 19) / lowerPart;
       } else {
-        lowerPart = 2 * FullMath.pow2(derivativeFx) - fx * doubleDerivativeFx;
         nextX = curX + FullMath.toPrecision(upperPart, 19) / lowerPart;
       }
 
@@ -56,61 +86,88 @@ contract PolySwapFormula {
 
   function appFormulaNewton(uint256[2][] calldata parameters, uint256 initialX)
     external
-    pure
+    view
     returns (uint256[] memory results)
   {
     results = new uint256[](100);
-    // uint256 nextX = FullMath.toPrecision(initialX, 18);
-    // uint256 curX;
-    // uint256 count = 0;
-    // do {
-    //   curX = nextX;
-    //   (uint256 fx, ) = f(parameters, k, curX, 36, 18);
-    //   uint256 derivativeFx = derivativeF(parameters, curX, 54, 18);
+    uint256 nextX = FullMath.toPrecision(initialX, 18);
+    uint256 curX;
+    uint256 count = 0;
+    do {
+      curX = nextX;
+      (uint256 f, bool isFNegative) = appF(parameters, curX, 36, 18);
+      uint256 dF = appDerivativeF(parameters, curX, 55, 18);
 
-    //   nextX = curX + FullMath.toPrecision(fx, 18) / derivativeFx;
-    //   results[count] = nextX;
-    //   count++;
-    // } while (curX != nextX);
-
-    uint256 curX = FullMath.toPrecision(initialX, 18);
-    (uint256 positiveF, uint256 negativeF) = appF(parameters, curX, 36, 18);
-    (uint256 positiveDerivativeF, uint256 negativeDerivativeF) = appDerivativeF(
-      parameters,
-      curX,
-      55,
-      18
-    );
-    (
-      uint256 positiveDoubleDerivativeFx,
-      uint256 negativeDoubleDerivativeFx
-    ) = appDoubleDerivativeF(parameters, curX, 73, 18);
-
-    results[0] = positiveF;
-    results[1] = negativeF;
-
-    results[2] = positiveDerivativeF;
-    results[3] = negativeDerivativeF;
-
-    results[4] = positiveDoubleDerivativeFx;
-    results[5] = negativeDoubleDerivativeFx;
-
-    unchecked {
-      if (
-        (positiveF > negativeF && positiveDerivativeF > negativeDerivativeF) ||
-        (positiveF < negativeF && positiveDerivativeF < negativeDerivativeF)
-      ) {
-        results[6] =
-          curX +
-          FullMath.toPrecision(FullMath.subAbs(positiveF, negativeF), 19) /
-          FullMath.subAbs(positiveDerivativeF, negativeDerivativeF);
+      if (isFNegative) {
+        unchecked {
+          nextX = curX - FullMath.toPrecision(f, 19) / dF;
+        }
       } else {
-        results[6] =
-          curX -
-          FullMath.toPrecision(FullMath.subAbs(positiveF, negativeF), 19) /
-          FullMath.subAbs(positiveDerivativeF, negativeDerivativeF);
+        nextX = curX + FullMath.toPrecision(f, 19) / dF;
       }
-    }
+
+      results[count] = nextX;
+      count++;
+    } while (curX != nextX);
+  }
+
+  function appFormulaHalley(uint256[2][] calldata parameters, uint256 initialX)
+    external
+    view
+    returns (uint256[] memory results)
+  {
+    results = new uint256[](100);
+    uint256 nextX = FullMath.toPrecision(initialX, 18);
+    uint256 curX;
+    uint256 count = 0;
+    do {
+      curX = nextX;
+      (uint256 fx, bool isFxNegative) = appF(parameters, curX, 36, 18);
+      uint256 derivativeFx = appDerivativeF(parameters, curX, 55, 18);
+      uint256 doubleDerivativeFx = appDoubleDerivativeF(
+        parameters,
+        curX,
+        74,
+        18
+      );
+
+      uint256 upperPart = 2 * fx * derivativeFx;
+      uint256 lowerPart;
+      bool lowerPartNegative;
+
+      if (isFxNegative) {
+        if (fx * doubleDerivativeFx >= 2 * FullMath.pow2(derivativeFx)) {
+          lowerPartNegative = false;
+        } else {
+          lowerPartNegative = true;
+        }
+
+        lowerPart = FullMath.subAbs(
+          fx * doubleDerivativeFx,
+          2 * FullMath.pow2(derivativeFx)
+        );
+      } else {
+        if (2 * FullMath.pow2(derivativeFx) >= fx * doubleDerivativeFx) {
+          lowerPartNegative = false;
+        } else {
+          lowerPartNegative = true;
+        }
+
+        lowerPart = FullMath.subAbs(
+          2 * FullMath.pow2(derivativeFx),
+          fx * doubleDerivativeFx
+        );
+      }
+
+      if (lowerPartNegative) {
+        nextX = curX - FullMath.toPrecision(upperPart, 19) / lowerPart;
+      } else {
+        nextX = curX + FullMath.toPrecision(upperPart, 19) / lowerPart;
+      }
+
+      results[count] = nextX;
+      count++;
+    } while (curX != nextX);
   }
 
   function f(
@@ -119,7 +176,7 @@ contract PolySwapFormula {
     uint256 x,
     uint256 decimals1,
     uint256 decimals2
-  ) public pure returns (uint256 result, bool isNegative) {
+  ) public view returns (uint256 result, bool isNegative) {
     uint256 length = parameters.length;
 
     for (uint256 i = 0; i < length; i++) {
@@ -130,15 +187,13 @@ contract PolySwapFormula {
       );
     }
 
-    uint256 kInPrecision = FullMath.toPrecision(k, 18);
-
-    if (result < kInPrecision) {
-      result = kInPrecision - result;
-      isNegative = true;
-    } else {
-      result = result - kInPrecision;
+    if (result >= FullMath.toPrecision(k, decimals2 - 1)) {
       isNegative = false;
+    } else {
+      isNegative = true;
     }
+
+    result = FullMath.subAbs(result, FullMath.toPrecision(k, decimals2 - 1));
   }
 
   function derivativeF(
@@ -182,34 +237,29 @@ contract PolySwapFormula {
     uint256 x,
     uint256 decimals1,
     uint256 decimals2
-  ) public pure returns (uint256 positive, uint256 negative) {
+  ) public view returns (uint256 result, bool isNegative) {
     for (uint256 i = 1; i < parameters.length; i++) {
-      (uint256 subtraction, bool isNegative) = FullMath.sub(
-        parameters[0][1],
-        parameters[i][1]
+      result += FullMath.mulDiv(
+        FullMath.toPrecision(parameters[i][0], decimals1),
+        parameters[0][1] - parameters[i][1],
+        FullMath.add(FullMath.toPrecision(parameters[i][1], decimals2), x)
       );
-
-      unchecked {
-        isNegative
-          ? negative += FullMath.mulDiv(
-            FullMath.toPrecision(parameters[i][0], decimals1),
-            subtraction,
-            FullMath.add(FullMath.toPrecision(parameters[i][1], decimals2), x)
-          )
-          : positive += FullMath.mulDiv(
-          FullMath.toPrecision(parameters[i][0], decimals1),
-          subtraction,
-          FullMath.add(FullMath.toPrecision(parameters[i][1], decimals2), x)
-        );
-
-        positive += FullMath.toPrecision(parameters[i][0], decimals2);
-      }
+      result += FullMath.toPrecision(parameters[i][0], decimals2);
     }
 
-    unchecked {
-      positive += FullMath.toPrecision(parameters[0][0], decimals2);
-      negative += (x + FullMath.toPrecision(parameters[0][1], decimals2));
+    if (
+      result + FullMath.toPrecision(parameters[0][0], decimals2) >=
+      x + FullMath.toPrecision(parameters[0][1], decimals2)
+    ) {
+      isNegative = false;
+    } else {
+      isNegative = true;
     }
+
+    result = FullMath.subAbs(
+      result + FullMath.toPrecision(parameters[0][0], decimals2),
+      x + FullMath.toPrecision(parameters[0][1], decimals2)
+    );
   }
 
   function appDerivativeF(
@@ -217,34 +267,17 @@ contract PolySwapFormula {
     uint256 x,
     uint256 decimals1,
     uint256 decimals2
-  ) public pure returns (uint256 positive, uint256 negative) {
+  ) public pure returns (uint256 result) {
     for (uint256 i = 1; i < parameters.length; i++) {
-      (uint256 subtraction, bool isNegative) = FullMath.sub(
-        parameters[0][1],
-        parameters[i][1]
+      result += FullMath.mulDiv(
+        FullMath.toPrecision(parameters[i][0], decimals1),
+        parameters[0][1] - parameters[i][1],
+        FullMath.pow2(
+          FullMath.add(FullMath.toPrecision(parameters[i][1], decimals2), x)
+        )
       );
-
-      unchecked {
-        isNegative
-          ? positive += FullMath.mulDiv(
-            FullMath.toPrecision(parameters[i][0], decimals1),
-            subtraction,
-            FullMath.pow2(
-              FullMath.add(FullMath.toPrecision(parameters[i][1], decimals2), x)
-            )
-          )
-          : negative += FullMath.mulDiv(
-          FullMath.toPrecision(parameters[i][0], decimals1),
-          subtraction,
-          FullMath.pow2(
-            FullMath.add(FullMath.toPrecision(parameters[i][1], decimals2), x)
-          )
-        );
-      }
     }
-    unchecked {
-      negative += FullMath.toPrecision(1, decimals2 + 1);
-    }
+    result += FullMath.toPrecision(1, decimals2 + 1);
   }
 
   function appDoubleDerivativeF(
@@ -252,30 +285,15 @@ contract PolySwapFormula {
     uint256 x,
     uint256 decimals1,
     uint256 decimals2
-  ) public pure returns (uint256 positive, uint256 negative) {
+  ) public pure returns (uint256 result) {
     for (uint256 i = 1; i < parameters.length; i++) {
-      (uint256 subtraction, bool isNegative) = FullMath.sub(
-        parameters[0][1],
-        parameters[i][1]
+      result += FullMath.mulDiv(
+        2 * FullMath.toPrecision(parameters[i][0], decimals1),
+        parameters[0][1] - parameters[i][1],
+        FullMath.pow3(
+          FullMath.add(FullMath.toPrecision(parameters[i][1], decimals2), x)
+        )
       );
-
-      unchecked {
-        isNegative
-          ? negative += FullMath.mulDiv(
-            2 * FullMath.toPrecision(parameters[i][0], decimals1),
-            subtraction,
-            FullMath.pow3(
-              FullMath.add(FullMath.toPrecision(parameters[i][1], decimals2), x)
-            )
-          )
-          : positive += FullMath.mulDiv(
-          2 * FullMath.toPrecision(parameters[i][0], decimals1),
-          subtraction,
-          FullMath.pow3(
-            FullMath.add(FullMath.toPrecision(parameters[i][1], decimals2), x)
-          )
-        );
-      }
     }
   }
 }
--- test/polySwap.ts
+++ test/polySwap.ts
@@ -16,17 +16,17 @@ describe("PolySwap", function () {
     BigNumber
   ][] = [
     [
+      [BigNumber.from(20), BigNumber.from(100)],
+      [BigNumber.from(30), BigNumber.from(40)],
+      [BigNumber.from(50), BigNumber.from(60)],
+      BigNumber.from(10),
+      BigNumber.from(5),
+    ],
+    [
       [BigNumber.from(30), BigNumber.from(400)],
       [BigNumber.from(50), BigNumber.from(60)],
       [BigNumber.from(20), BigNumber.from(100)],
-      BigNumber.from(5),
-      BigNumber.from(40),
-    ],
-    [
-      [BigNumber.from(30), BigNumber.from(40)],
-      [BigNumber.from(50), BigNumber.from(60)],
-      [BigNumber.from(20), BigNumber.from(100)],
-      BigNumber.from(15),
+      BigNumber.from(10),
       BigNumber.from(5),
     ],
   ];
@@ -42,46 +42,63 @@ describe("PolySwap", function () {
     await contract.deployed();
   });
 
-  // it("PolySwap: Newton Method", async function () {
-  //   const results = await Promise.all(
-  //     testCases.map(async (testCase): Promise<BigNumber[]> => {
-  //       const results = await contract.standardFormulaNewton(
-  //         testCase.slice(0, 3) as [[BigNumber, BigNumber]],
-  //         testCase[3],
-  //         testCase[4]
-  //       );
+  it("PolySwap: Newton Method", async function () {
+    const results = await Promise.all(
+      testCases.map(async (testCase): Promise<BigNumber[]> => {
+        const results = await contract.standardFormulaNewton(
+          testCase.slice(0, 3) as [[BigNumber, BigNumber]],
+          testCase[3],
+          testCase[4]
+        );
 
-  //       return results.filter((item: BigNumber) => item.toString() !== "0");
-  //     })
-  //   );
+        return results.filter((item: BigNumber) => item.toString() !== "0");
+      })
+    );
 
-  //   console.log(results);
+    console.log(results);
 
-  //   expect(true).to.be.equals(true);
-  // });
+    expect(true).to.be.equals(true);
+  });
 
-  // it("PolySwap: Halley Method", async function () {
-  //   const results = await Promise.all(
-  //     testCases.map(async (testCase): Promise<BigNumber[]> => {
-  //       const results = await contract.standardFormulaHalley(
-  //         testCase.slice(0, 3) as [[BigNumber, BigNumber]],
-  //         testCase[3],
-  //         testCase[4]
-  //       );
+  it("PolySwap: Halley Method", async function () {
+    const results = await Promise.all(
+      testCases.map(async (testCase): Promise<BigNumber[]> => {
+        const results = await contract.standardFormulaHalley(
+          testCase.slice(0, 3) as [[BigNumber, BigNumber]],
+          testCase[3],
+          testCase[4]
+        );
 
-  //       return results.filter((item: BigNumber) => item.toString() !== "0");
-  //     })
-  //   );
+        return results.filter((item: BigNumber) => item.toString() !== "0");
+      })
+    );
 
-  //   console.log(results);
+    console.log(results);
 
-  //   expect(true).to.be.equals(true);
-  // });
+    expect(true).to.be.equals(true);
+  });
 
   it("PolySwap: Approximation Formula Newton Method", async function () {
     const results = await Promise.all(
       testCases.map(async (testCase): Promise<BigNumber[]> => {
         const results = await contract.appFormulaNewton(
+          testCase.slice(0, 3) as [[BigNumber, BigNumber]],
+          testCase[4]
+        );
+
+        return results.filter((item: BigNumber) => item.toString() !== "0");
+      })
+    );
+
+    console.log(results);
+
+    expect(true).to.be.equals(true);
+  });
+
+  it("PolySwap: Approximation Formula Halley Method", async function () {
+    const results = await Promise.all(
+      testCases.map(async (testCase): Promise<BigNumber[]> => {
+        const results = await contract.appFormulaHalley(
           testCase.slice(0, 3) as [[BigNumber, BigNumber]],
           testCase[4]
         );
